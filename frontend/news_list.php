61a32c5400cb888579a8d2f6c82856014121cdcd
aliciacher <32888887+aliciacher@users.noreply.github.com>
2017-10-19 16:30:44 +0800

    1.0.3
    第二版

--- frontend/news_list.php
+++ frontend/news_list.php
@@ -3,7 +3,7 @@ require_once("../connection/database.php");
 $sth = $db->query("SELECT * FROM news ORDER BY published_date DESC");
 $all_news = $sth->fetchAll(PDO::FETCH_ASSOC);
 $totalRows = count($all_news);
-?>
+ ?>
 <!doctype html>
 <!-- Website template by freewebsitetemplates.com -->
 <html>
@@ -27,26 +27,26 @@ $totalRows = count($all_news);
 					<ul>
 						<?php foreach($all_news as $row){ ?>
 						<li>
-							<img src="../images/new-chills.png" alt="">
+
 							<div>
 								<h1><?php echo $row['title']; ?></h1>
 								<span><?php echo $row['published_date']; ?></span>
-								<p><?php echo mb_substr( $row['content'],0,50,"utf-8")."..."; ?></p>
-								<a href="news.php?newsID=<?php echo $row['newsID'];?>" class="more">Read More</a>
+								<p><?php echo mb_substr($row['content'],0,50,'utf-8'); ?></p>
+								<a href="news.php?newsID=<?php echo $row['newsID']; ?>" class="more">Read More</a>
 							</div>
 						</li>
-					<?php } ?>
+						<?php } ?>
 						<li>
 							<img src="../images/berries.png" alt="">
 							<div>
 								<h1>BERRIES ON THE GROVE</h1>
 								<span>By Admin on November 28, 2023</span>
 								<p>You can replace all this text with your own text. You can remove any link to our website from this website template, you're free to use this website template without linking back to us. If you're having problems editing this website template.</p>
-								<a href="news" class="more">Read More</a>
+								<a href="news.php" class="more">Read More</a>
 							</div>
 						</li>
 					</ul>
-					<a href="news_list.php" class="load">Load More</a>
+					<a href="blog.php" class="load">Load More</a>
 				</div>
 				<div class="sidebar">
 					<h1>Recent Posts</h1>
@@ -54,7 +54,7 @@ $totalRows = count($all_news);
 					<h2>ON THE DIET</h2>
 					<span>By Admin on November 28, 2023</span>
 					<p>You can replace all this text with your own text. You can remove any link to our website from this website template.</p>
-					<a href="news" class="more">Read More</a>
+					<a href="news.php" class="more">Read More</a>
 				</div>
 			</div>
 		</div>
